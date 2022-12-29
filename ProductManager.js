b05c3d59b25c1dec277d2d7f814e3b6c89656b4d
joeldrivero <joelriv98@gmail.com>
2022-12-28 23:38:00 -0300

    Entregable 2

--- ProductManager.js
+++ ProductManager.js
@@ -1,42 +1,77 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor(productos = []) {
+    constructor(productos = [], path) {
         this.productos = productos;
+        this.path = path;
     }
+
     static id = 0;
 
-    addProducts = (title, description, price, thumbnail, code, stock) => {
-        if (title == "" || description == "" || price == null || thumbnail == "" || code == "" || stock == null) {
+    addProducts = (productoNuevo) => {
+        if (!productoNuevo.title || !productoNuevo.description || !productoNuevo.price || !productoNuevo.thumbnail || !productoNuevo.code || !productoNuevo.stock) {
             return console.error("Por favor, complete todos los campos obligatorios");
         }
 
-        if (this.productos.length != 0) {
-            for (let i = 0; i < this.productos.length; i++) {
-                if (this.productos[i].code == code)
-                    return console.error("El producto ya existe");
-            }
-            this.productos.push({ title: title, description: description, price: price, thumbnail: thumbnail, code: code, stock: stock, id: ProductManager.id })
-            ProductManager.id++;
-        }
-        else {
-            this.productos.push({ title: title, description: description, price: price, thumbnail: thumbnail, code: code, stock: stock, id: ProductManager.id })
-            ProductManager.id++;
-        }
-
+        if (this.productos.find(prod => prod.code === productoNuevo.code)) return console.error("El producto ya existe");
+        this.productos.push({ title: productoNuevo.title, description: productoNuevo.description, price: productoNuevo.price, thumbnail: productoNuevo.thumbnail, code: productoNuevo.code, stock: productoNuevo.stock, id: ProductManager.id })
+        fs.writeFileSync(this.path, JSON.stringify(this.productos));
+        ProductManager.id++;
     }
 
     getProducts() {
-        return this.productos;
+        return JSON.parse(fs.readFileSync(this.path, 'utf-8'));
     }
 
     getProductsById(id) {
-        if (this.productos.length != 0) {
-            for (let i = 0; i < this.productos.length; i++) {
-                if (this.productos[i].id == id)
-                    return this.productos[i];
-            }
+        const prodArchivo = this.getProducts();
+        const prod = prodArchivo.find(prod => prod.id === id)
+        return prod ? prod : console.log("Not Found");
+
+    };
+
+    updateProduct(producto, id) {
+
+        const prodArchivo = this.getProducts();
+        const prod = prodArchivo.find(prod => prod.id === id)
+        console.log(producto.title)
+        console.log(this.productos)
+
+        if (producto.title && prod.title != producto.title) {
+            prod.title = producto.title;
         }
-        return console.error("Not found")
+
+        if (producto.description && prod.description != producto.description) {
+            prod.description = producto.description;
+        }
+
+        if (producto.price && prod.price != producto.price) {
+            prod.price = producto.price;
+        }
+
+        if (producto.thumbnail && prod.thumbnail != producto.thumbnail) {
+            prod.thumbnail = producto.thumbnail;
+        }
+
+        if (producto.code && prod.code != producto.code) {
+            if (prodArchivo.find(prod => prod.code === producto.code)) return console.error("El producto ya existe");
+            prod.code = producto.code;
+        }
+
+        if (producto.stock && prod.stock != producto.stock) {
+            prod.stock = producto.stock;
+        }
+
+        fs.writeFileSync(this.path, JSON.stringify(prodArchivo));
+    };
+
+    deleteProduct(id) {
+        const prodArchivo = this.getProducts();
+        const prod = prodArchivo.filter(prod => prod.id != id)
+        prod ? fs.writeFileSync(this.path, JSON.stringify(prod)) : console.log("Not Found");
     };
 
 
 }
+
+const productos = new ProductManager([], "./productos.json");
